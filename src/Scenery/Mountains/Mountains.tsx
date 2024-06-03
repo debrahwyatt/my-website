b8c914c902908ad7774ee74c365b7d1fb28764e1
Debrah Wyatt <debrahrwyatt@gmail.com>
2024-06-02 21:35:28 -0600

    added night time transition for moutnains and hill

--- src/Scenery/Mountains/Mountains.tsx
+++ src/Scenery/Mountains/Mountains.tsx
@@ -5,21 +5,21 @@ const Mountains = () => {
 
     return (
       <div id="mountains">
-        <div className="mountain-two">
+        <div className="mountain1">
           <div className="mountain-top">
             <div className="mountain-cap-1"></div>
             <div className="mountain-cap-2"></div>
             <div className="mountain-cap-3"></div>
           </div>
         </div>
-        <div className="mountain">
+        <div className="mountain2">
           <div className="mountain-top">
             <div className="mountain-cap-1"></div>
             <div className="mountain-cap-2"></div>
             <div className="mountain-cap-3"></div>
           </div>
         </div>
-        <div className="mountain-three">
+        <div className="mountain3">
           <div className="mountain-top">
             <div className="mountain-cap-1"></div>
             <div className="mountain-cap-2"></div>
