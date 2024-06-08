97c769cbffba5203d98c3f50e8e6838352ceed86
Debrah Wyatt <debrahrwyatt@gmail.com>
2024-06-08 15:37:50 -0600

    mobile scenery updates

--- src/Scenery/Hills/Hills.tsx
+++ src/Scenery/Hills/Hills.tsx
@@ -4,9 +4,9 @@ const Hills = () => {
 
     return (
       <div id="hills">
-        <div className="hill-2 hill"/>
+        <div className="hill-1 hill"/>
         <div className="stream hill"/>
-        <div className="hill-3 hill"/>
+        <div className="hill-2 hill"/>
       </div>
     );
 }
