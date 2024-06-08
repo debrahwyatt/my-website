8f776a896c88ed540e86d09311f1c49a360df4a2
Debrah Wyatt <debrahrwyatt@gmail.com>
2024-06-08 15:13:26 -0600

    made the site more mobile friendly

--- src/Content/About/AboutContent.tsx
+++ src/Content/About/AboutContent.tsx
@@ -1,8 +1,20 @@
+import pic1 from './pic1.jpg'
 
 export const AboutContent = () => {
   return (
     <div>
-      Hello and welcome to my webpage! My name is Debrah Wyatt, I'm a software engineer with a specialization in artificial intelligence. <br/><br/>
+      <div>
+        <img src={pic1} alt="system diagram" className='pic1' />
+      </div>
+      <div>
+        <p>
+          My name is Debrah Wyatt, I'm a software engineering graduate from the University of Victoria with a specialization in artificial intelligence. While my experience is mostly front-end web development using React, Typescript, HTML, and JavaScript; I'm also proficient in back-end technology with a specialization in artificial intelligence. <br/>
+          <br/>
+          I'm a full stack web developer with experience in LAMP (Linux, Apache, MySQL, and PHP), and I've hosted various websites from my home server. My personal projects either contain some entrepreneurial element or exist to enhance my personal repertoire of skills.<br/>
+          <br/>
+          Overall, I take my work seriously when it comes to implementation but I'm all for having fun while that happens. Looking forward to bridging connections, growing projects, and meeting new people.
+        </p>
+      </div>
     </div>
   )
 }
