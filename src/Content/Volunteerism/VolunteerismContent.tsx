caa73a461722752a08170f124da35ade9ab0dd70
Debrah Wyatt <debrahrwyatt@gmail.com>
2024-06-09 21:59:28 -0600

    basic content added to Volunteerism and Work Experience, formatted Project content.

--- src/Content/Volunteerism/VolunteerismContent.tsx
+++ src/Content/Volunteerism/VolunteerismContent.tsx
@@ -2,7 +2,19 @@
 export const VolunteerismContent = () => {
   return (
     <div>
-
+      Volunteer, APEGA<br/>
+      Grande Prairie, AB • Feb 2023 – Present<br/>
+      •	Setup and take down of event activities.<br/>
+      •	Facilitated the development of STEM skills in elementary students.<br/>
+      <br/>
+      President/Founder, UVic AI Club<br/>
+      Victoria, BC • Sept 2019 – Sept 2022 <br/>
+      •	Developed and hosted the club website.<br/>
+      •	Procured monetary resources for club treasury.<br/>
+      •	Registered the club with over 100 initial signatures.<br/>
+      •	Participated with the club in Battlesnake tournaments.<br/>
+      •	Built a chatbot for the club website using Jaccard similarity.<br/>
+      •	Coordinated club meetings and activities to foster engagement.<br/>
     </div>
   )
 }
--- src/Content/WorkExperience/WorkExperienceContent.tsx
+++ src/Content/WorkExperience/WorkExperienceContent.tsx
@@ -2,6 +2,33 @@
 export const WorkExperienceContent = () => {
   return (
     <div>
+      Business Systems Programmer, Northwestern Polytechnic<br/>
+      Grande Prairie, AB • Sept 2022 - Present<br/>
+      •	Mapped the business systems software architecture.<br/>
+      •	Management of large-scale organizational projects.<br/>
+      •	Worked to resolve interdepartmental software issues.<br/>
+      •	Wrote department guides, plans, policies, and procedures.<br/>
+      •	Conducted software related upgrades on critical infrastructure.<br/>
+      •	Planned, organized, and orchestrated a disaster recovery simulation.  <br/>
+      <br/>
+      Software Developer, UVic Drug Checking Program<br/>
+      Victoria, BC • Sept 2021 – Dec 2021 <br/>
+      •	Fixed bugs and optimized UI systems.<br/>
+      •	Developed tools to further expand the usability of LEAPUS.<br/>
+      •	Resolved development issues using Scrum, Kanban, and Agile.<br/>
+      <br/>
+      Front-End Developer, Rockland Scientific<br/>
+      Victoria, BC • Sept 2021 – Dec 2021 <br/>
+      •	Incorporated documentation both written and UML.<br/>
+      •	Improved application performance for product review.<br/>
+      •	Implemented foundational features to enhance user experience.<br/>
+      •	Optimised, refactored, and updated code for agile development.<br/>
+      <br/>
+      Software Developer, Sure Grip Controls<br/>
+      Saanichton, BC • Sept 2020 – Dec 2020 <br/>
+      •	Utilized NetSuite’s API to mine relevant data.<br/>
+      •	Developed automation tools to assist in production. <br/>
+      •	Created integrated systems for terminals, printers, and databases.<br/>
     </div>
   )
 }
