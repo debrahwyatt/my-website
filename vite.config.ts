eb53fdb1af9cb3ee2766757b0c014c4c185b8b5a
Debrah Wyatt <debrahrwyatt@gmail.com>
2024-06-11 19:06:25 -0600

    pages still broken 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-website/docs/', 
+  base: '/my-website/', 
   build: {
     outDir: 'docs'
   }
